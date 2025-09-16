49af368e12db2ab8e076cdd57e0be643510b3d73
imjuyongp <imjuyongp@gmail.com>
2025-09-16 17:21:33 +0900

    :tada:Start: Initial commit

---
+++ src/main/java/com/skthon/sixthsensebe/SixthsenseBeApplication.java
@@ -0,0 +1,13 @@
+package com.skthon.sixthsensebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixthsenseBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SixthsenseBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/skthon/sixthsensebe/SixthsenseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skthon.sixthsensebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixthsenseBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
