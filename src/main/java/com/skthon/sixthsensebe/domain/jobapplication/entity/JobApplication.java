fc2dc527e10ccd1cc60f1463114bcf71affaaffd
imjuyongp <imjuyongp@gmail.com>
2025-09-17 21:11:07 +0900

    :truck:Rename: 패키지 기본구조

---
+++ src/main/java/com/skthon/sixthsensebe/domain/jobapplication/entity/JobApplication.java
@@ -0,0 +1,4 @@
+package com.skthon.sixthsensebe.domain.jobapplication.entity;
+
+public class JobApplication {
+}
---
+++ src/main/java/com/skthon/sixthsensebe/domain/jobposting/entity/JobPosting.java
@@ -0,0 +1,4 @@
+package com.skthon.sixthsensebe.domain.jobposting.entity;
+
+public class JobPosting {
+}
