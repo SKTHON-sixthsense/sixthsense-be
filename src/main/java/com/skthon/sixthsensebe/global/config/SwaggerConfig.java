9c5e59935d3568d73d308c2eaf3ec2f31df63eae
shinchaerin79 <shinchaerin79@gmail.com>
2025-09-18 20:55:11 +0900

    :wrench: Settings: Swagger에 JWT Bearer 인증 설정 추가

--- src/main/java/com/skthon/sixthsensebe/global/config/SwaggerConfig.java
+++ src/main/java/com/skthon/sixthsensebe/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.skthon.sixthsensebe.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,18 +19,34 @@ public class SwaggerConfig {
 
   @Bean
   public OpenAPI customOpenAPI() {
+    String securitySchemeName = "bearerAuth";
+
     Server localServer = new Server();
-    localServer.url(contextPath);
+    localServer.setUrl(contextPath);
     localServer.setDescription("Local Server");
 
     return new OpenAPI()
         .addServersItem(localServer)
-        .info(new Info().title("Swagger API 명세서").version("1.0").description("SKTHON-sixthsense"));
+        .info(new Info()
+            .title("Swagger API 명세서")
+            .version("1.0")
+            .description("SKTHON-sixthsense"))
+        // Bearer 토큰 인증 설정
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+        .components(new Components()
+            .addSecuritySchemes(securitySchemeName,
+                new SecurityScheme()
+                    .name(securitySchemeName)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
   }
 
   @Bean
   public GroupedOpenApi customGroupedOpenApi() {
-    return GroupedOpenApi.builder().group("api").pathsToMatch("/**").build();
+    return GroupedOpenApi.builder()
+        .group("api")
+        .pathsToMatch("/**")
+        .build();
   }
 }
-
