5ecb1df24c664bf1f0fb3ca74d1a4cae3e63c6ee
imjuyongp <imjuyongp@gmail.com>
2025-09-20 01:22:25 +0900

    :bug:Fix: request dto add annotation

--- src/main/java/com/skthon/sixthsensebe/global/naverocr/dto/response/OcrResponse.java
+++ src/main/java/com/skthon/sixthsensebe/global/naverocr/dto/response/OcrResponse.java
@@ -2,9 +2,17 @@ package com.skthon.sixthsensebe.global.naverocr.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class OcrResponse {
 
   @Schema(description = "이미지 인식 결과", example = "SUCCESS", allowableValues = {"SUCCESS", "FAILURE", "ERROR"})
