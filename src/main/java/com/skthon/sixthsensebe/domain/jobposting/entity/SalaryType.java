d8358dc74d4dedb09d354963fb489f916aedab03
imjuyongp <imjuyongp@gmail.com>
2025-09-23 18:35:44 +0900

    :sparkles:Feat: 급여 형태 enum클래스 추가

--- src/main/java/com/skthon/sixthsensebe/domain/jobposting/entity/SalaryType.java
+++ src/main/java/com/skthon/sixthsensebe/domain/jobposting/entity/SalaryType.java
@@ -1,4 +1,15 @@
 package com.skthon.sixthsensebe.domain.jobposting.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SalaryType {
+  WEEKLYPAY("시급"),
+  MONTHLYPAY("월급");
+
+  @JsonValue
+  private final String value;
 }
